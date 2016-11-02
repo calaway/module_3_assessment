ac8697ce163b61e3d25bb895a529eef162cb3c1a
Calaway <Christopher.Calaway@gmail.com>
2016-11-02 10:55:34 -0600

    Fill out store model.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 class SearchController < ApplicationController
   def index
+    require "pry"; binding.pry
+    zip = params
     conn = Faraday.new(:url => 'https://api.bestbuy.com/') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
-    response = conn.get "/v1/stores(area(80202,100))?format=json&show=longName,city,distance,phone,storeType&pageSize=15&apiKey=#{ENV["API_KEY"]}"
+    response = conn.get "/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=15&apiKey=#{ENV["API_KEY"]}"
 
     raw_stores = JSON.parse(response.body)["stores"]
 
--- spec/features/user_can_consume_api_spec.rb
+++ spec/features/user_can_consume_api_spec.rb
@@ -12,13 +12,13 @@ RSpec.feature "" do
       # Then my current path should be "/search" (ignoring params)
       expect(current_path).to eq("/search")
       # And I should see stores within 25 miles of 80202
-      expect(page).to have_content("store1")
+      expect(page).to have_content("stores within 25 miles of 80202")
       # And I should see a message that says "17 Total Stores"
       expect(page).to have_content("17 Total Stores")
       # And I should see exactly 15 results
       expect(page).to have_content("15 stores")
       # And I should see the long name, city, distance, phone number and store type for each of the 15 results
-      expect(page).to have_content
+      expect(page).to have_content("Cherry Creek Shopping Center")
     end
   end
 end
