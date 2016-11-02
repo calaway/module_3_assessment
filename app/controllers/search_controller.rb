70c8583e284d93a5ccc946ebf28567ed8c17eac5
Calaway <Christopher.Calaway@gmail.com>
2016-11-02 11:48:04 -0600

    Refactor search box.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,6 @@ class SearchController < ApplicationController
   def index
     raw_stores = StoreLocationService.fetch(params["zip"])
 
-
     @stores = Store.return_stores(raw_stores, params["zip"])
   end
 end
