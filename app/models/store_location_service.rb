95a316031210be23e9c6d3fa40dc1a581e7523b1
Calaway <Christopher.Calaway@gmail.com>
2016-11-02 11:21:20 -0600

    Make feature test more robust.

--- app/models/store_location_service.rb
+++ app/models/store_location_service.rb
@@ -4,8 +4,19 @@ module StoreLocationService
       faraday.adapter  Faraday.default_adapter
     end
 
+    conn.get do |req|         # GET http://sushi.com/search?page=2&limit=100
+      req.url '/v1/stores(area(80202,25))', :page => 2
+      req.params['format'] = 'json'
+      req.params['show'] = 'longName,city,distance,phone,storeType'
+      req.params['pageSize'] = '50'
+      req.params['apiKey'] = ENV["API_KEY"]
+    end
 
-    response = conn.get "/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=50&apiKey=#{ENV["API_KEY"]}"
+    # response = conn.get "/v1/stores(area(80202,25))?
+    # format=json
+    # show=longName,city,distance,phone,storeType
+    # pageSize=50
+    # apiKey=#{ENV["API_KEY"]}"
 
     JSON.parse(response.body)["stores"]
   end
