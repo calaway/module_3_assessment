1a77936869d8d8d85664b2722d9b3375749c8649
Calaway <Christopher.Calaway@gmail.com>
2016-11-02 10:42:09 -0600

    Start creating store model.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,11 @@
 class Store
-  def return_stores
+  def initialize(raw_store)
+    require "pry"; binding.pry
+  end
 
+  def return_stores(raw_stores)
+    raw_stores.map do |raw_store|
+      new(raw_store)
+    end
   end
 end
