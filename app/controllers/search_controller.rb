0425c1a70a5b8469e4e4cef7a5a3b650c034675c
Calaway <Christopher.Calaway@gmail.com>
2016-11-02 11:02:29 -0600

    Start building out search index view.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,12 @@
 class SearchController < ApplicationController
   def index
-    require "pry"; binding.pry
-    zip = params
+    @zip = params["q"]
     conn = Faraday.new(:url => 'https://api.bestbuy.com/') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
-    response = conn.get "/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=15&apiKey=#{ENV["API_KEY"]}"
+    response = conn.get "/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=50&apiKey=#{ENV["API_KEY"]}"
 
     raw_stores = JSON.parse(response.body)["stores"]
 
