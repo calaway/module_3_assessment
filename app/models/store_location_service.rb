fd1f213da8fb496bb7405905a7c59eb8c7a0bec0
Calaway <Christopher.Calaway@gmail.com>
2016-11-02 11:25:36 -0600

    Refactor faraday service.

--- app/models/store_location_service.rb
+++ app/models/store_location_service.rb
@@ -1,23 +1,21 @@
 module StoreLocationService
   def self.fetch(zip)
-    conn = Faraday.new(:url => 'https://api.bestbuy.com/') do |faraday|
+    JSON.parse(response.body)["stores"]
+  end
+
+  def self.conn
+    Faraday.new(:url => 'https://api.bestbuy.com/') do |faraday|
       faraday.adapter  Faraday.default_adapter
     end
+  end
 
-    conn.get do |req|         # GET http://sushi.com/search?page=2&limit=100
-      req.url '/v1/stores(area(80202,25))', :page => 2
+  def self.response
+    response = conn.get do |req|
+      req.url '/v1/stores(area(80202,25))'
       req.params['format'] = 'json'
       req.params['show'] = 'longName,city,distance,phone,storeType'
       req.params['pageSize'] = '50'
       req.params['apiKey'] = ENV["API_KEY"]
     end
-
-    # response = conn.get "/v1/stores(area(80202,25))?
-    # format=json
-    # show=longName,city,distance,phone,storeType
-    # pageSize=50
-    # apiKey=#{ENV["API_KEY"]}"
-
-    JSON.parse(response.body)["stores"]
   end
 end
