79bac8add7abfa608599f7a5be1494736d40c893
Calaway <Christopher.Calaway@gmail.com>
2016-11-02 09:52:20 -0600

    Stub out feature test for consuming an api.

--- spec/features/user_can_consume_api_rspec.rb
+++ spec/features/user_can_consume_api_rspec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 RSpec.feature "" do
   it "" do
     VCR.use_cassette("consuming_api") do
-      
+      # As a user
+      # When I visit "/"
+      visit "/"
+      # And I fill in a search box with "80202" and click "search"
+      fill_in "search_box", with: "80202"
+      click_on "search"
+      # Then my current path should be "/search" (ignoring params)
+      expect(current_path).to be("/search")
+      # And I should see stores within 25 miles of 80202
+      expect(page).to have_content("store1")
+      # And I should see a message that says "17 Total Stores"
+      expect(page).to have_content("17 Total Stores")
+      # And I should see exactly 15 results
+      expect(page).to have_content("15 stores")
+      # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+      expect(page).to have_content
     end
   end
 end
